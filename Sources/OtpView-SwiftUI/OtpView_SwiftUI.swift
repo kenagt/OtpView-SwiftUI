dbdfc43e3ce647050243c3df7666801ff52327d7
Kenan <begic_kenan@hotmail.com>
2022-04-22 15:16:06 +0200

    - implementing package

--- Sources/OtpView-SwiftUI/OtpView_SwiftUI.swift
+++ Sources/OtpView-SwiftUI/OtpView_SwiftUI.swift
@@ -1,6 +1,83 @@
-public struct OtpView_SwiftUI {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import Combine
 
-    public init() {
+@available(iOS 15, *)
+struct OtpView_SwiftUI: View {
+    
+    //MARK: Fields
+    enum FocusField: Hashable {
+        case field
+    }
+    @FocusState private var focusedField: FocusField?
+    @Binding var otpCode: String
+    @Binding var otpCodeLengh: Int
+    @Binding var textColor: Color
+    @Binding var textSize: CGFloat
+    
+    //MARK: Constructor
+    public init(otpCode: Binding<String>, otpCodeLengh: Binding<Int>, textColor: Binding<Color>, textSize: Binding<CGFloat>) {
+        self._otpCode = otpCode
+        self._otpCodeLengh = otpCodeLengh
+        self._textColor = textColor
+        self._textSize = textSize
+    }
+    
+    //MARK: Body
+    var body: some View {
+        HStack {
+            ZStack(alignment: .center) {
+                TextField("", text: $otpCode)
+                    .frame(width: 0, height: 0, alignment: .center)
+                    .font(Font.system(size: 0))
+                    .accentColor(.clear)
+                    .foregroundColor(.clear)
+                    .multilineTextAlignment(.center)
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(otpCode)) { _ in limitText(otpCodeLengh) }
+                    .focused($focusedField, equals: .field)
+                    .task {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)
+                        {
+                            self.focusedField = .field
+                        }
+                    }
+                    .padding()
+                HStack {
+                    ForEach(0..<otpCodeLengh) { index in
+                        ZStack {
+                            Text(self.getPin(at: index))
+                                .font(Font.system(size: textSize))
+                                .fontWeight(.semibold)
+                                .foregroundColor(textColor)
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundColor(textColor)
+                                .padding(.trailing, 5)
+                                .padding(.leading, 5)
+                                .opacity(self.otpCode.count <= index ? 1 : 0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func getPin(at index: Int) -> String {
+        guard self.otpCode.count > index else {
+            return ""
+        }
+        return self.otpCode[index]
+    }
+    
+    private func limitText(_ upper: Int) {
+        if otpCode.count > upper {
+            otpCode = String(otpCode.prefix(upper))
+        }
+    }
+}
+
+extension String {
+    subscript(idx: Int) -> String {
+        String(self[index(startIndex, offsetBy: idx)])
     }
 }
--- Tests/OtpView-SwiftUITests/OtpView_SwiftUITests.swift
+++ Tests/OtpView-SwiftUITests/OtpView_SwiftUITests.swift
@@ -6,6 +6,6 @@ final class OtpView_SwiftUITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(OtpView_SwiftUI().text, "Hello, World!")
+        //XCTAssertEqual(OtpView_SwiftUI().text, "Hello, World!")
     }
 }
